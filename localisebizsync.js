4123fd9dc9618b2e243b530c07dca009cf646c70
christian <chamelin@greenflex.com>
2018-11-08 18:29:47 +0100

    move extract to project

--- localisebizsync.js
+++ localisebizsync.js
@@ -13,57 +13,60 @@ let version = "1.0.7-stable";
 let cmdValue = null;
 
 program
-        .version(version)
-        .option("-v, --verbose", "display verbose")
-        .option("-f, --from [language]", "language from extract")
-        .option(
-                "-d, --direction [direction]",
-                "use 'down' if local changes should be overwritten [default: 'down'] ( use for sync cmd )"
-                )
-        .usage("[options] <cmd>")
-        .arguments("<cmd>")
-        .action(function (cmd, env) {
-            cmdValue = cmd;
-        });
+  .version(version)
+  .option("-v, --verbose", "display verbose")
+  .option("-f, --from [language]", "language from extract")
+  .option("-ri, --react-intl", "use file from babel-plugin-react-intl extract")
+  .option(
+    "-d, --direction [direction]",
+    "use 'down' if local changes should be overwritten [default: 'down'] ( use for sync cmd )"
+  )
+  .usage("[options] <cmd>")
+  .arguments("<cmd>")
+  .action(function(cmd, env) {
+    cmdValue = cmd;
+  });
 
 init();
 
 function init() {
-    program.on("--help", function () {
-        console.log("  Infos:");
-        console.log("\tWrite configuration in config.yaml file. Open README.md");
-        console.log("\n  Commandes:");
-        console.log("\tsync \t synchronize translation with localise.biz");
-        console.log("\textract  extract key from language for others");
-        console.log(
-                "\tdownload downlaod translations file from localise.biz to local pathToTranslations"
-                );
-        console.log("\n  Usages:");
-        console.log("\tlocalizediff sync");
-        console.log("\tlocalizediff -v -d up sync");
-        console.log("\tlocalizediff -f en extract");
-        console.log("\tlocalizediff -v -f fr extract");
-        console.log("\tlocalizediff -v download");
-    });
-    program.parse(process.argv);
+  program.on("--help", function() {
+    console.log("  Infos:");
+    console.log("\tWrite configuration in localize.yaml file. Open README.md");
+    console.log("\n  Commandes:");
+    console.log("\tsync \t synchronize translation with localise.biz");
+    console.log("\textract  extract key from language for others");
+    console.log(
+      "\tdownload downlaod translations file from localise.biz to local pathToTranslations"
+    );
+    console.log("\n  Usages:");
+    console.log("\tlocalizediff sync");
+    console.log("\tlocalizediff sync -ri");
+    console.log("\tlocalizediff -v -d up sync");
+    console.log("\tlocalizediff -f en extract");
+    console.log("\tlocalizediff -v -f fr extract");
+    console.log("\tlocalizediff -v download");
+  });
+  program.parse(process.argv);
 
-    switch (cmdValue) {
-        case "sync":
-            program.verbose ? console.log("\n") : "";
-            syncProgram.start(
-                    program.verbose,
-                    program.direction ? program.direction : "down"
-                    );
-            break;
-        case "extract":
-            program.verbose ? console.log("\n") : "";
-            extractProgram.start(program.verbose, program.from);
-            break;
-        case "download":
-            program.verbose ? console.log("\n") : "";
-            downloadProgram.start(program.verbose);
-            break;
-        default:
-            program.verbose ? console.warn("WARN ::::: No command") : "";
-    }
+  switch (cmdValue) {
+    case "sync":
+      program.verbose ? console.log("\n") : "";
+      syncProgram.start(
+        program.verbose,
+        program.direction ? program.direction : "down",
+        program.reactIntl
+      );
+      break;
+    case "extract":
+      program.verbose ? console.log("\n") : "";
+      extractProgram.start(program.verbose, program.from);
+      break;
+    case "download":
+      program.verbose ? console.log("\n") : "";
+      downloadProgram.start(program.verbose);
+      break;
+    default:
+      program.verbose ? console.warn("WARN ::::: No command") : "";
+  }
 }
