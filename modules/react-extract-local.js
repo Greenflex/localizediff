63a0898df38c54df5faed898febcfc10822471be
svalencia <svalencia@greenflex.com>
2019-04-04 19:21:36 +0200

    Upgrade to babel7 (typescript support)

--- modules/react-extract-local.js
+++ modules/react-extract-local.js
@@ -1,25 +1,33 @@
 const fs = require("fs");
 const R = require("ramda");
 const glob = require("glob");
-const babel = require("babel-core");
+const babel = require("@babel/core");
+const path = require("path");
 
 process.env.NODE_ENV = "production"; // For babel.transform
 
 function extractMessages(pattern /* : string */) /* : string */ {
+  console.log(pattern);
   const srcPaths = glob.sync(pattern, { absolute: true });
   const relativeSrcPaths = glob.sync(pattern);
-  const contents = srcPaths.map(p => fs.readFileSync(p, "utf-8"));
+  const contents = srcPaths
+    .filter(p => {
+      const ext = path.extname(p);
+      return (
+        ext === ".js" || ext === ".ts" || (ext === ".tsx") | (ext === ".jsx")
+      );
+    })
+    .map(p => [p, fs.readFileSync(p, "utf-8")]);
+
   const messages = contents
-    .map(content =>
-      babel.transform(content, {
-        presets: [
-          require.resolve("babel-preset-react-app"),
-          require.resolve("babel-preset-stage-1")
-        ],
-        plugins: [require.resolve("babel-plugin-react-intl")],
+    .map(content => {
+      return babel.transform(content[1], {
+        filename: content[0],
+        presets: [["react-app", { flow: true, typescript: true }]],
+        plugins: ["babel-plugin-react-intl"],
         babelrc: false
-      })
-    )
+      });
+    })
     .map(R.path(["metadata", "react-intl", "messages"]));
 
   const result = R.zipWith(
